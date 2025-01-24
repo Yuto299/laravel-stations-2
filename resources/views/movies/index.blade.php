831637ae8f1ab4867f5be7df94fccefdb83bd257
Yuto299 <ota.se.ke@outlook.jp>
2025-01-24 17:45:14 +0900

    7-

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>映画一覧</title>
+</head>
+
+<body>
+  <ul>
+    @foreach ($movies as $movie)
+    <li>{{ $movie->id }}</li>
+    <li>{{ $movie->title }}</li>
+    <li>{{ $movie->image_url }}</li>
+    <li><img src="{{ $movie->image_url }}" alt="{{ $movie->title }}" width="300"></li>
+    <li>{{ $movie->created_at }}</li>
+    <li>{{ $movie->updated_at }}</li>
+    @endforeach
+  </ul>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PracticeController;
 
@@ -19,4 +20,4 @@ Route::get('/practice', [PracticeController::class, 'sample']);
 Route::get('/practice2', [PracticeController::class, 'sample2']);
 Route::get('/practice3', [PracticeController::class, 'sample3']);
 Route::get('/getPractice', [PracticeController::class, 'getPractice']);
-Route::get('/movies', [PracticeController::class, 'movies']);
+Route::get('/movies', [MovieController::class, 'index']);
